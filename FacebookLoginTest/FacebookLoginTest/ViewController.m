4b9fcdd2dd56b133295b99f95e04aada70c4f32f
Daisuke Hirata <daisukihirata@gmail.com>
2015-05-17 19:15:50 +0900

    Add Facebook login button to ViewController

--- FacebookLoginTest/FacebookLoginTest/ViewController.m
+++ FacebookLoginTest/FacebookLoginTest/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface ViewController ()
 
@@ -16,12 +18,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
 }
 
 @end
