6dc1c00283683e4169f7b4d4511ef7450a722eb7
Daisuke Hirata <daisukihirata@gmail.com>
2015-05-17 19:02:54 +0900

    initial commit

---
+++ FacebookLoginTest/FacebookLoginTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacebookLoginTest
+//
+//  Created by Daisuke Hirata on 5/17/15.
+//  Copyright (c) 2015 Daisuke Hirata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacebookLoginTest/FacebookLoginTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacebookLoginTest
+//
+//  Created by Daisuke Hirata on 5/17/15.
+//  Copyright (c) 2015 Daisuke Hirata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
